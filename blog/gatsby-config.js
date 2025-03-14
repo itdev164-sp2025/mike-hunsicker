7f74712d183a9b987ec84a30179628467c27d7b3
Mike Hunsicker <hunsickm@matc.edu>
2025-03-13 19:41:40 -0500

    One more change to demo activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -13,7 +13,7 @@ require(`dotenv`).config({
  */
 module.exports = {
   siteMetadata: {
-    title: `My Blog`,
+    title: `My Sweet Blog`,
     description: ``,
     author: `Mike Hunsicker`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
